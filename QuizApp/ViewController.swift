c5269b348f8262e213b421f7bb7dfe086be7f5c1
Adnan Sarker <adnansarker92@gmail.com>
2021-03-27 23:50:51 +0600

    add 2D array for question memory

--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var questionText: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let quiz = [
+        ["hi, im your bro", "True"],
+        ["Hey u r my bro", "False"],
+        ["its raining", "True"]
+    ]
+    //let size = quiz.count
+    var quesNum = 0
+    @IBAction func ansButton(_ sender: UIButton) {
+        let actualAns = quiz[quesNum][1]
+        let userAns = sender.currentTitle!
+        
+        if userAns == actualAns {
+            print("Right")
+        }
+        else {
+            print ("Wrong")
+        }
+        quesNum = quesNum + 1
+        updateUI()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
-
+    
+    func updateUI () {
+        if quesNum > quiz.count - 1 {
+            quesNum = 0
+        }
+        questionText.text = quiz [quesNum][0]
+        
+    }
 
 }
 
