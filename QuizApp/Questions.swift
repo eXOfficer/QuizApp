2ca02beb68609700c0543ea4e8f3bf0f36a5d0b8
Adnan Sarker <adnansarker92@gmail.com>
2021-03-28 01:31:31 +0600

    add Questions using 'struct'

---
+++ QuizApp/Questions.swift
@@ -0,0 +1,14 @@
+//
+//  Questions.swift
+//  QuizApp
+//
+//  Created by Adnan Sarker on 28/03/2021.
+//  Copyright © 2021 Justdreams. All rights reserved.
+//
+
+import Foundation
+
+struct Questions {
+    let text: String
+    let answer: String
+}
--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -3,8 +3,8 @@
 //  QuizApp
 //
 //  Created by Adnan Sarker on 27/03/2021.
+//  Copyright © 2021 Justdreams. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -16,15 +16,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var progressBar: UIProgressView!
     
     let quiz = [
-        ["hi, im your bro", "True"],
-        ["Hey u r my bro", "False"],
-        ["its raining", "True"]
+        Questions(text: "hi, im your bro", answer: "True"),
+        Questions(text: "hey u r my bro", answer: "False"),
+        Questions(text: "its raining", answer: "True")
     ]
     //let size = quiz.count
     var quesNum = 0
     @IBAction func ansButton(_ sender: UIButton) {
-        let actualAns = quiz[quesNum][1]
-        let userAns = sender.currentTitle!
+        let actualAns = quiz[quesNum].answer
+        let userAns = sender.currentTitle!// True or False
         
         if userAns == actualAns {
             print("Right")
@@ -45,7 +45,7 @@ class ViewController: UIViewController {
         if quesNum > quiz.count - 1 {
             quesNum = 0
         }
-        questionText.text = quiz [quesNum][0]
+        questionText.text = quiz [quesNum].text
         
     }
 
